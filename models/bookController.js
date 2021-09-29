2bb8887703933e5dc61d69c2a7836ea501bd186d
Hesham-Hassan9 <19012748@student.ltuc.com>
2021-09-29 17:37:22 +0300

    lab14

--- models/bookController.js
+++ models/bookController.js
@@ -1,54 +1,85 @@
-'use strict';
+"use strict";
 
-const {   books } = require("./books.js");
+const { books } = require("./books.js");
 
 let bookHandler = (req, res) => {
-    let email1 = req.query.email;
-      books.find({email:email1}).then(data => {
-        res.json(data); 
+  let email1 = req.query.email;
+  books
+    .find({ email: email1 })
+    .then((data) => {
+      res.json(data);
     })
     .catch((error) => {
-        res.statu(500).send('error there is no data to get');
+      res.statu(500).send("error there is no data to get");
     });
-}
+};
 
-let createHandler = async  (req, res) => {
-    let { title, author, description, status, email} = req.body;
-    // await   books.create({
-    //   title: title1,
-    //   author: author1,
-    //   description: description1,
-    //   status: status1,
-    //   email: email1
-    // })
+let createHandler = async (req, res) => {
+  let { title, author, description, status, email } = req.body;
+  // await   books.create({
+  //   title: title1,
+  //   author: author1,
+  //   description: description1,
+  //   status: status1,
+  //   email: email1
+  // })
 
-    await   books.create(req.body)
+  await books.create(req.body);
 
-      books.find({email:email}).then(data => {
-        res.status(200).json(data);
-    }).catch((error) => {
-        res.status(500).send('error there is no recived data');
+  books
+    .find({ email: email })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      res.status(500).send("error there is no recived data");
     });
-}
+};
 
 let deleteHandler = (req, res) => {
-
-    // let id = req.query.id;
-    let id = req.params.bookID;
-    console.log(id);
-    let email1 = req.query.email;
-    console.log(email1);
-      books.findByIdAndDelete({_id:id}).then(() => {
-          books.find({email:email1}).then(data => {
-            res.status(200).json(data);
-        })
-    }).catch((error) => {
-        res.status(500).send('error there is no file');
+  // let id = req.query.id;
+  let id = req.params.bookID;
+  console.log(id);
+  let email1 = req.query.email;
+  console.log(email1);
+  books
+    .findByIdAndDelete({ _id: id })
+    .then(() => {
+      books.find({ email: email1 }).then((data) => {
+        res.status(200).json(data);
+      });
+    })
+    .catch((error) => {
+      res.status(500).send("error there is no file");
     });
+};
+
+function updateHandler(req, res) {
+    console.log(req.body);
+  let { title, author, description, status, id, email1 } = req.body;
+  books.findByIdAndUpdate(
+    id,
+    { title, author, description, status },
+    (error, updatedData) => {
+      if (error) {
+        console.log("error in updating");
+      } else {
+        console.log("updatedData", updatedData);
+        books.find({ email: email1 }, function (error, bookData) {
+          if (error) {
+            console.log("error in getting data", error);
+          } else {
+            res.send(bookData);
+          }
+        });
+      }
+    }
+  );
 }
 
 module.exports = {
-    bookHandler,
-    createHandler,
-    deleteHandler,
-}
+  bookHandler,
+  createHandler,
+  deleteHandler,
+  updateHandler,
+};
--- server.js
+++ server.js
@@ -12,13 +12,14 @@ const MONGO_SERVER_LINK = process.env.MONGO_SERVER_LINK
 
 mongoose.connect(`${MONGO_SERVER_LINK}`);
 
-const { bookHandler, createHandler, deleteHandler,} = require("./models/bookController");
+const { bookHandler, createHandler, deleteHandler, updateHandler} = require("./models/bookController");
 
 // Routes
 server.get('/', homeRouteHandler);
 server.get('/Book', bookHandler);
 server.post('/create-book', createHandler);
 server.delete('/delete-book/:bookID', deleteHandler);
+server.put('/update-book', updateHandler);
 server.get('*', notFoundHandler);
 
 // Function Handlers
@@ -35,50 +36,3 @@ function homeRouteHandler(req, res) {
   });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "use strict";
-
-// const express = require("express");
-// const server = express();
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-// server.use(cors());
-
-// const PORT = process.env.PORT;
-// const MONGO_SERVER_LINK = process.env.MONGO_SERVER_LINK;
-
-// mongoose.connect(`${MONGO_SERVER_LINK}`);
-
-// const { getBookHandler, createBookController } = require("./models/bookController");
-
-// server.get("/", homeRouteHandler);
-// server.get("/getBook", getBookHandler);
-// app.post('/create-data', createBookController);
-// server.get("*", notFoundHandler);
-
-// function homeRouteHandler(req, res) {
-//   res.send("home route");
-// }
-
-// function notFoundHandler(req, res) {
-//   res.status(404).send("NOT FOUND!!");
-// }
-
-// server.listen(PORT, () => {
-//   console.log(`Listening on PORT ${PORT}`);
-// });
